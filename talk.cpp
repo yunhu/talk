e5ee54430485f21cd5dd97170b0e0e4a0dcbcb5c
root <root@ubuntu.ubuntu-domain>
2015-07-23 16:39:15 +0800

    mysql

--- talk.cpp
+++ talk.cpp
@@ -33,47 +33,26 @@ void talk::on_pushButton_2_clicked() {
       QMessageBox::warning(this,"Error","用户名和密码不能为空",QMessageBox::Close);
  }else{
      //connect mysql
-  //   qDebug()<<QCoreApplication::libraryPaths ();
-           QSqlDatabase db( QSqlDatabase::addDatabase( "QSQLITE" ) );
-            db.setDatabaseName("talk.db");
-            db.setUserName("root");
-            db.setPassword("tiger");
+     qDebug()<<QCoreApplication::libraryPaths ();
 
 
-            if(!db.open())/*测试数据库是否链接成功*/
-            {
-               QMessageBox::critical(0, QObject::tr("Databse Error"), db.lastError().text());
-               qDebug()<<db.lastError()<<endl;
-            }
-            else
-            {
-                 QSqlQuery sql_query;
-                   //     QString table =  "create table user (id int primary key, name varchar(30), pass varchar(30))";
-                 QString insert = "insert into user values(3, 'zhangyh2','tigera')";
-                 QString update = "update user set name='zhangyunhu' where id = 1";
-                 QString del = "delete from user where name ='zhangyh'";
 
-                 QString select_all = "select * from user";
+     QSqlDatabase db( QSqlDatabase::addDatabase( "QMYSQL" ) );
+             db.setHostName("localhost");
+             db.setDatabaseName("qt");
+             db.setUserName("root");
+             db.setPassword("tier");
 
-                        sql_query.prepare(select_all);
 
-                       // sql_query.addBindValue(1);
-                        //sql_query.addBindValue("tiger");
-                        //sql_query.addBindValue("tigera");
-                        if(!sql_query.exec())
-                        {
-                            qDebug()<<sql_query.lastError();
-                        }
-                        else
-                        {
-                            while(sql_query.next()){
-                                int id = sql_query.value(0).toInt();
-                                QString name = sql_query.value(1).toString();
-                                QString pass   = sql_query.value(2).toString();
-                                qDebug()<< id << name << pass;
-                            }
-                        }
-            }
+             if(db.open())/*测试数据库是否链接成功*/
+             {
+                QMessageBox::critical(0, QObject::tr("Databse Error"), db.lastError().text());
+                qDebug()<<db.lastError()<<endl;
+             }
+             else
+             {
+                qDebug()<<"Ok"<<endl;
+             }
  }
 
 }
